445f0aafee2df30bbc632d93e33586ba502badf9
vishnureddy <tarigopula34@gmail.com>
2024-10-15 14:35:58 +0530

    The fronted scripts has been executed successfully.

---
+++ 02.print.sh
@@ -0,0 +1,18 @@
+#print some message on the screen  we use echo or print command.
+#echo is simple approch &widely used ,we wikly learn it.
+
+#echo input
+echo hello world
+
+#print colors
+#systex: echo -e "\e[COLmHello world\e[0m"
+#echo -e , -e - enable colors
+#\e[COLm -tell which color
+#\e[0m -disable color
+#color        code
+#Red          31
+#green        32
+#yellow       33
+#blue         34
+#meganta      35
+#cyan         36
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # roboshop-shell
+
+shell scripting 
+
+linux does not require file extenstion.
+
+
+
+### sed command 
+syntext : sed -i -e operation2 files
+Ex:sed -i -e 'root/ d' -e'2d file
+Note:if we don't give 'i' option then it will make changes on the output not on the file
