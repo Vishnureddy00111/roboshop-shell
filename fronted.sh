41daa6a63af3b8684845dcb9e991948175cb947b
vishnureddy <tarigopula34@gmail.com>
2025-03-04 20:03:56 +0530

    fronted script is executed

--- fronted.sh
+++ fronted.sh
@@ -1,7 +1,6 @@
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
-systemctl enable nginx
 
 cp nginx.conf /etc/nginx/nginx.conf
 
@@ -10,5 +9,8 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+
+
 systemctl restart nginx
+systemctl enable nginx
 
