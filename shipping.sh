8d16272c8ec679443d8344b09b211442e70730ba
vishnureddy <tarigopula34@gmail.com>
2025-03-04 18:19:15 +0530

    shell

--- shipping.sh
+++ shipping.sh
@@ -11,14 +11,16 @@ mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 systemctl daemon-reload
 
-systemctl enable shipping
-systemctl start shipping
 
 dnf install mysql -y
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h MYSQL.vishnureddy.online -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h MYSQL.vishnureddy.online -uroot -pRoboShop@1 < /app/db/app-user.sql
+mysql -h MYSQL.vishnureddy -uroot -pRoboShop@1 < /app/db/master-data.sql
 
+
+
+systemctl enable shipping
+systemctl start shipping
 systemctl restart shipping
 
